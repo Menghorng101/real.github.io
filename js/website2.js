1212854cc65f517fc6026562dee301a1d3bfbf4a
Menghorng101 <145375971+Menghorng101@users.noreply.github.com>
2023-09-23 14:39:05 +0700

    Add files via upload

---
+++ js/website2.js
@@ -0,0 +1,14 @@
+// alert("hello world!");
+// const hamburgarBtn = document.querySelector(".hamburgar");
+// const menuBar = document.querySelector(".responsive-navbar");
+
+// hamburgarBtn.addEventListener("click",function (){
+//     menuBar.classList.toggle("active");
+// });
+
+const hambugarBtn = document.querySelector(".hambugar");
+const menuBar = document.querySelector(".responsive-navbar");
+
+hambugarBtn.addEventListener("click",function (){
+    menuBar.classList.toggle("active");
+});
